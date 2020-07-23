f217b40cfb98d0e79d24328a70e98288b284ae9d
Marius Bruehlmeier <mariusbr@ethz.ch>
2020-07-23 09:28:12 +0200

    compiling code with commented tf interfaces

--- segmatch/include/segmatch/descriptors/cnn.hpp
+++ segmatch/include/segmatch/descriptors/cnn.hpp
@@ -2,7 +2,7 @@
 #define SEGMATCH_CNN_HPP_
 
 #include <laser_slam/common.hpp>
-#include <tf_graph_executor/tf_graph_executor.hpp>
+// #include <tf_graph_executor/tf_graph_executor.hpp>
 
 #include "segmatch/descriptors/descriptors.hpp"
 #include "segmatch/parameters.hpp"
@@ -18,9 +18,9 @@ class CNNDescriptor : public Descriptor {
     const std::string semantics_nn_folder = parameters.semantics_nn_path;
 
     LOG(INFO) << "Loading CNN model in " + model_folder;
-    graph_executor_.reset(new tf_graph_executor::TensorflowGraphExecutor(
-        model_folder + "model.ckpt.meta"));
-    graph_executor_->loadCheckpoint(model_folder + "model.ckpt");
+    // graph_executor_.reset(new tf_graph_executor::TensorflowGraphExecutor(
+    //     model_folder + "model.ckpt.meta"));
+    // graph_executor_->loadCheckpoint(model_folder + "model.ckpt");
 
     aligned_segments_ = SegmentedCloud(false);
 
@@ -36,9 +36,9 @@ class CNNDescriptor : public Descriptor {
 
     // Load the semantics nn model.
     LOG(INFO) << "Loading semantics model in " + semantics_nn_folder;
-    semantics_graph_executor_.reset(new tf_graph_executor::TensorflowGraphExecutor(
-        semantics_nn_folder + "model.ckpt.meta"));
-    semantics_graph_executor_->loadCheckpoint(semantics_nn_folder + "model.ckpt");
+    // semantics_graph_executor_.reset(new tf_graph_executor::TensorflowGraphExecutor(
+    //     semantics_nn_folder + "model.ckpt.meta"));
+    // semantics_graph_executor_->loadCheckpoint(semantics_nn_folder + "model.ckpt");
 
     LOG(INFO) << "Loaded all TensorFlow models.";
   }
@@ -65,8 +65,8 @@ class CNNDescriptor : public Descriptor {
 
   DescriptorsParameters params_;
 
-  std::shared_ptr<tf_graph_executor::TensorflowGraphExecutor> graph_executor_;
-  std::shared_ptr<tf_graph_executor::TensorflowGraphExecutor> semantics_graph_executor_;
+  // std::shared_ptr<tf_graph_executor::TensorflowGraphExecutor> graph_executor_;
+  // std::shared_ptr<tf_graph_executor::TensorflowGraphExecutor> semantics_graph_executor_;
 
   std::vector<float> voxel_mean_values_;
 
