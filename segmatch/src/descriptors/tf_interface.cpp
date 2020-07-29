aea868b39d5c26dc8c08853f6735d7c59ea7cf95
Marius Bruehlmeier <mariusbr@ethz.ch>
2020-07-29 11:49:12 +0200

    added tensorflow input to message

--- segmatch/src/descriptors/tf_interface.cpp
+++ segmatch/src/descriptors/tf_interface.cpp
@@ -30,28 +30,57 @@ void TensorflowInterface::batchFullForwardPass(
   msg.descriptor_values_name = descriptor_values_name;
   msg.reconstruction_values_name = reconstruction_values_name;
 
-  // ------------------------------------
-  // Encode Multiarrays to message format
-  // ------------------------------------
+  std_msgs::MultiArrayDimension layout_dim;
 
   // Scales
-  std_msgs::MultiArrayDimension scales_layout_dim;
-  scales_layout_dim.size = scales.size();
-  scales_layout_dim.stride = scales.size() * scales[0].size();
-  msg.scales.layout.dim.push_back(scales_layout_dim);
+  layout_dim.size = scales.size();
+  layout_dim.stride = scales.size() * scales[0].size();
+  msg.scales.layout.dim.push_back(layout_dim);
 
-  scales_layout_dim.size = scales[0].size();
-  scales_layout_dim.stride = scales[0].size();
-  msg.scales.layout.dim.push_back(scales_layout_dim);
+  layout_dim.size = scales[0].size();
+  layout_dim.stride = scales[0].size();
+  msg.scales.layout.dim.push_back(layout_dim);
 
   for (int i = 0; i < msg.scales.layout.dim[0].size; ++i) {
     for (int j = 0; j < msg.scales.layout.dim[1].size; ++j) {
       msg.scales.data.push_back(scales[i][j]);
     }
-    ROS_INFO_STREAM(scales[i][0] << " " << scales[i][1] << " " << scales[i][2]);
+    // ROS_INFO_STREAM(scales[i][0] << " " << scales[i][1] << " " <<
+    // scales[i][2]);
   }
 
-  msg.timestamp = ros::Time::now().toNSec();
+  // Inputs
+  size_t dim_1 = inputs.size();
+  size_t dim_2 = inputs[0].container.size();
+  size_t dim_3 = inputs[0].container[0].size();
+  size_t dim_4 = inputs[0].container[0][0].size();
+  layout_dim.size = dim_1;
+  layout_dim.stride = dim_1 * dim_2 * dim_3 * dim_4;
+  msg.inputs.layout.dim.push_back(layout_dim);
+
+  layout_dim.size = dim_2;
+  layout_dim.stride = dim_2 * dim_3 * dim_4;
+  msg.inputs.layout.dim.push_back(layout_dim);
+
+  layout_dim.size = dim_3;
+  layout_dim.stride = dim_3 * dim_4;
+  msg.inputs.layout.dim.push_back(layout_dim);
+
+  layout_dim.size = dim_4;
+  layout_dim.stride = dim_4;
+  msg.inputs.layout.dim.push_back(layout_dim);
+
+  for (int i = 0; i < msg.inputs.layout.dim[0].size; ++i) {
+    for (int j = 0; j < msg.inputs.layout.dim[1].size; ++j) {
+      for (int k = 0; k < msg.inputs.layout.dim[2].size; ++k) {
+        for (int l = 0; l < msg.inputs.layout.dim[3].size; ++l) {
+          msg.inputs.data.push_back(inputs[i].container[j][k][l]);
+        }
+      }
+    }
+  }
+  auto msg_time_stamp = ros::Time::now().toNSec();
+  msg.timestamp = msg_time_stamp;
   ROS_INFO_STREAM("sending at: " << msg.timestamp);
   publisher_batch_full_forward_pass_.publish(msg);
   ros::Rate loop_rate(10);
--- segmatch/src/descriptors/tf_interface.py
+++ segmatch/src/descriptors/tf_interface.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from __future__ import print_function
+# from __future__ import print_function
 from array import *
 import rospy
 from segmatch.msg import batch_full_forward_pass_msg
@@ -7,11 +7,11 @@ from numpy import empty
 
 
 def batch_full_forward_pass_callback(msg):
-    rospy.loginfo('I heard %s', msg.timestamp)
-
-    # ------------------------
-    # Decoding of ROS message
-    # ------------------------
+    # rospy.loginfo('I heard %s', msg.timestamp)
+    input_tensor_name = msg.input_tensor_name
+    scales_tensor_name = msg.scales_tensor_name
+    descriptor_values_name = msg.descriptor_values_name
+    reconstruction_values_name = msg.reconstruction_values_name
 
     # Scales
     scales = empty([msg.scales.layout.dim[0].size,
@@ -21,14 +21,36 @@ def batch_full_forward_pass_callback(msg):
         for j in range(0, msg.scales.layout.dim[1].size):
             scales[i][j] = msg.scales.data[msg.scales.layout.dim[1].stride * i + j]
 
-    for r in scales:
-        for c in r:
-            print(c, end=" ")
-        print()
+        # Inputs
+    inputs = empty([msg.inputs.layout.dim[0].size, msg.inputs.layout.dim[1].size,
+                    msg.inputs.layout.dim[2].size, msg.inputs.layout.dim[3].size])
+
+    stride_1 = msg.inputs.layout.dim[1].stride
+    stride_2 = msg.inputs.layout.dim[2].stride
+    stride_3 = msg.inputs.layout.dim[3].stride
+
+    for i in range(0, msg.inputs.layout.dim[0].size):
+        for j in range(0, msg.inputs.layout.dim[1].size):
+            for k in range(0, msg.inputs.layout.dim[2].size):
+                for l in range(0, msg.inputs.layout.dim[3].size):
+                    inputs[i][j][k][l] = msg.inputs.data[i *
+                                                         stride_1 + j*stride_2 + k * stride_3 + l]
+
+    # for r in scales:
+    #     for c in r:
+    #         print(c, end=" ")
+    #     print()
+    batch_full_forward_pass_tf_interface(
+        inputs, scales, input_tensor_name, scales_tensor_name, descriptor_values_name, reconstruction_values_name)
+
+
+def batch_full_forward_pass_tf_interface(inputs, scales, input_tensor_name,
+scales_tensor_name, descriptor_values_name, reconstruction_values_name):
+    print('ola')
 
 
 def listener():
-    rospy.init_node('listener', anonymous=True)
+    rospy.init_node('listener', anonymous = True)
     rospy.Subscriber('tf_interface_topic/batch_full_forward_pass_topic',
                      batch_full_forward_pass_msg, batch_full_forward_pass_callback)
     rospy.spin()
