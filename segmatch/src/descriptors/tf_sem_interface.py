24fae5ded6c38f9d79ac62255a946f38c46ac2fd
Marius Bruehlmeier <mariusbr@ethz.ch>
2020-08-19 13:40:15 +0200

    changed gpu memory issue

--- segmatch/src/descriptors/tf_sem_interface.py
+++ segmatch/src/descriptors/tf_sem_interface.py
@@ -78,9 +78,8 @@ class TensorflowSemanticsInterface:
         self.sem_output = sem_graph.get_tensor_by_name(
             'OutputScope/output_read:0')
         gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.4)
-        self.cnn_sess = tf.compat.v1.Session(
-            config=tf.ConfigProto(gpu_options=gpu_options))ons))
-        self.sem_session = tf.compat.v1.Session()
+        self.sem_sess = tf.compat.v1.Session(
+            config=tf.ConfigProto(gpu_options=gpu_options))
         self.saver.restore(self.sem_session, tf.train.latest_checkpoint(
             self.semantics_model_path))
         rospy.spin()
