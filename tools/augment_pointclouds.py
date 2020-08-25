73bfbdee415f49cb91773a32b86632097ff9b51f
Marius Bruehlmeier <mariusbr@ethz.ch>
2020-08-25 14:17:13 +0200

    added bosch data augmentation script

--- tools/augment_pointclouds.py
+++ tools/augment_pointclouds.py
@@ -4,29 +4,31 @@
 import numpy
 import os
 import yaml
+import struct
 import tf.transformations as transformations
 import rosbag
 from sensor_msgs import point_cloud2
+from sensor_msgs.msg import PointCloud2, PointField
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 
 
 def main():
 
-    with open(r'/home/marius/segmap_ws/src/segmap/segmapper/launch/bosch/segmentation_id_color.yaml') as file:
-        segmentation_ids = yaml.load(file, Loader=yaml.FullLoader)
-
-    bag_file = '/home/marius/.segmap/bosch/bosch.bag'
-    # bag_file = '/media/scratch1/bosch/bosch.bag'
-    out_bag_file = '/home/marius/.segmap/bosch/augmented_bosch.bag'
-    # out_bag_file = '/media/scratch1/bosch/augmented_bosch.bag'
+    # bag_file = '/home/marius/.segmap/bosch/bosch.bag'
+    bag_file = '/media/scratch1/bosch/bosch.bag'
+    # out_bag_file = '/home/marius/.segmap/bosch/augmented_bosch.bag'
+    out_bag_file = '/media/scratch1/bosch/augmented_bosch.bag'
     bag = rosbag.Bag(bag_file)
     out_bag = rosbag.Bag(out_bag_file, 'w')
-    image_width = 640
-    image_height = 480
-    f_x = image_width/2
-    f_y = image_width/2
-    c_x = image_width/2
-    c_y = image_height/2
+    image_width = 640.0
+    image_height = 480.0
+    f_x = image_width / 2.0
+    f_y = image_width / 2.0
+    c_x = image_width / 2.0
+    c_y = image_height / 2.0
+    camera_intrinsics = [[f_x, 0.0, c_x, 0.0], [
+        0.0, f_y, c_y, 0.0], [0.0, 0.0, 1.0, 0.0]]
 
     tf_drone_cam = transformations.quaternion_matrix(numpy.array(
         [-0.5, 0.5, -0.5, 0.5]))
@@ -47,16 +49,19 @@ def main():
 
     i = 0
 
-    for topic, image, t in bag.read_messages(topics='/airsim_drone/Depth_cam'):
+    for topic, image, t in bag.read_messages(topics='/airsim_drone/Scene_cam'):
+
         images.append(image)
 
         i += 1
-        if i == 40:
-            break
+        print('Image: ' + str(i))
 
     image_iterator = 0
     i = 0
     for topic, lidar_pcl, t in bag.read_messages(topics=['/airsim_drone/Lidar']):
+
+        augmented_points = []
+
         while(images[image_iterator].header.stamp < lidar_pcl.header.stamp and image_iterator < len(images)-1):
             image_iterator += 1
         current_image = images[image_iterator]
@@ -64,24 +69,65 @@ def main():
         j = 0
         for point in points:
             label = point[3]
-            point_exp = (point[0], point[1], point[2], 1)
-            tf_point_exp = numpy.dot(tf_cam_lidar, point_exp)
-            tf_point = (tf_point_exp[0], tf_point_exp[1], tf_point_exp[2])
-            print(point)
-            print(tf_point)
-            print(label)
-            j += 1
-            if j == 5:
-                break
+            projected_point = numpy.dot(
+                tf_cam_lidar, (point[0], point[1], point[2], 1))
+            r = 255
+            g = 255
+            b = 0
 
+            if (projected_point[2] > 0):
+                camera_point = numpy.dot(camera_intrinsics, projected_point)
+                image_coordinates = [
+                    camera_point[0] / camera_point[2], camera_point[1] / camera_point[2]]
 
+                if image_coordinates[0] > 0 and image_coordinates[0] < image_width - 1:
+                    if image_coordinates[1] > 0 and image_coordinates[1] < image_height - 1:
+                        u = int(round(image_coordinates[0]))
+                        v = int(round(image_coordinates[1]))
+                        bridge = CvBridge()
+                        cv_image = bridge.imgmsg_to_cv2(
+                            current_image, desired_encoding='passthrough')
+                        b = cv_image[v, u, 0]
+                        g = cv_image[v, u, 1]
+                        r = cv_image[v, u, 2]
+
+            rgba = struct.unpack('I', struct.pack(
+                'BBBB', b, g, r, int(label) + 200))[0]
+            augmented_points.append([point[0], point[1], point[2], rgba])
+
+        fields = [PointField('x', 0, PointField.FLOAT32, 1),
+                  PointField('y', 4, PointField.FLOAT32, 1),
+                  PointField('z', 8, PointField.FLOAT32, 1),
+                  PointField('rgba', 12, PointField.UINT32, 1),
+                  ]
+        header = lidar_pcl.header
+        augmented_cloud = point_cloud2.create_cloud(
+            header, fields, augmented_points)
+
+        out_bag.write('/augmented_cloud', augmented_cloud,
+                      augmented_cloud.header.stamp, False)
+        out_bag.write('/airsim_drone/Lidar', lidar_pcl,
+                      lidar_pcl.header.stamp, False)
 
         i += 1
-        if i == 5:
-            break
+        print('Pointcloud: ' + str(i))
+    i = 0
+    for topic, tf, t in bag.read_messages(topics=['/tf']):
+        out_bag.write('/tf', tf, tf.transforms[0].header.stamp, False)
+        i += 1
+        print('TF: ' + str(i))
 
-        print('-------------')
-    return
+    for topic, tf_static, t in bag.read_messages(topics=['/tf_static']):
+        out_bag.write('/tf_static', tf_static,
+                      tf_static.transforms[0].header.stamp, False)
+
+    i = 0
+    for topic, msg, t in bag.read_messages(topics=['/airsim_drone/RGBD_cam']):
+        out_bag.write('/airsim_drone/RGBD_cam', msg, msg.header.stamp, False)
+        i += 1
+        print('Depth Cam: ' + str(i))
+
+    out_bag.close()
 
 
 if __name__ == '__main__':
