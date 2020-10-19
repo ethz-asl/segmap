98778e575aa9b0017f5d4ce084aec0e51959e213
Nikhilesh Alatur <nikhilesh.alatur@ethz-asl.ch>
2020-10-19 17:26:31 +0200

    Fix: Fixed Eigen assignment problem (matrix to vector), which was causing bad_alloc().

--- segmatch/src/descriptors/fpfh.cpp
+++ segmatch/src/descriptors/fpfh.cpp
@@ -10,6 +10,7 @@
 #include <pcl/point_types.h>
 #include <pcl/features/normal_3d.h>
 #include <ctime>
+#include <cmath>
 
 #pragma STDC FENV_ACCESS on
 
@@ -58,16 +59,7 @@ void FpfhDescriptor::describe(const Segment& segment, Features* features) {
   pcl::Normal centroid_normal(0.0,0.0,1.0);
   pcl::PointXYZ centroid1(centroid.x, centroid.y, centroid.z);
   
-  // Add centroid at the end of point cloud and surface normal.
-  // std::cout<<"Centroid: X: "<<centroid1.x<<" Y: "<<centroid1.y<<" Z: "<<centroid1.z<<std::endl;
-  // std::cout<<"FirstEntry: X: "<<cloud->begin()->x<<" Y: "<<cloud->begin()->y<<" Z: "<<cloud->begin()->z<<std::endl;
-  // std::cout<<"LastEntry: X: "<<(cloud->end()-1)->x<<" Y: "<<(cloud->end()-1)->y<<" Z: "<<(cloud->end()-1)->z<<std::endl;
-  // std::cout<<"Cloud Size: "<<cloud->size()<<std::endl;
-
   cloud->push_back(centroid1);
-  // std::cout<<"FirstEntry: X: "<<cloud->begin()->x<<" Y: "<<cloud->begin()->y<<" Z: "<<cloud->begin()->z<<std::endl;
-  // std::cout<<"LastEntry: X: "<<(cloud->end()-1)->x<<" Y: "<<(cloud->end()-1)->y<<" Z: "<<(cloud->end()-1)->z<<std::endl;
-  // std::cout<<"Cloud Size: "<<cloud->size()<<std::endl;
 
   cloud_normals->push_back(centroid_normal);
 
@@ -87,7 +79,6 @@ void FpfhDescriptor::describe(const Segment& segment, Features* features) {
   Eigen::Vector4f max_pt_eig;
   pcl::getMaxDistance(*cloud, centroid1.getVector4fMap(), max_pt_eig);
   float max_distance = (Eigen::Vector3f(centroid1.x, centroid1.y, centroid1.z) - Eigen::Vector3f(max_pt_eig[0], max_pt_eig[1], max_pt_eig[2])).norm();
-  // std::cout<<"Max distance: "<<max_distance<<std::endl;
 
   // Set radius-search to allow for all points.
   fpfh.setRadiusSearch(1.1*max_distance);
@@ -96,36 +87,44 @@ void FpfhDescriptor::describe(const Segment& segment, Features* features) {
   std::vector<int> indices(cloud->size()-1);  // We don't want to include the last point, which is the centroid.
   std::iota(std::begin(indices), std::end(indices), 0);
 
+  std::cout<<"Test 5!"<<std::endl;
+
   int nr_subdiv = 11; // ToDo(alaturn) Make param.
   Eigen::MatrixXf hist_f1(1, nr_subdiv), hist_f2(1, nr_subdiv), hist_f3(1, nr_subdiv); 
+  Eigen::MatrixXf hist_tot(1, 3*nr_subdiv);
   // Check that last entry = centroid.
   fpfh.computePointSPFHSignature(*cloud, *cloud_normals, int(cloud->size()-1), 0, indices, hist_f1, hist_f2, hist_f3);
-  // std::cout<<" F1: "<<hist_f1.size()<<" F2: "<<hist_f2.size()<<" F3: "<<hist_f3.size()<<std::endl;
-  // fpfh.compute (*fpfhs);
-  // std::cout<<"Numbers: "<<cloud->size()<<" "<<fpfhs->size()<<std::endl;
+  hist_tot << hist_f1, hist_f2, hist_f3;
+  Eigen::VectorXf lol(3*nr_subdiv);
+  lol = hist_tot.row(0);
 
-  // Return descriptor.
-  Eigen::VectorXf fpfh_vec(hist_f1.size() + hist_f2.size() + hist_f3.size());
-  fpfh_vec << hist_f1, hist_f2, hist_f3;
+  std::cout<<"Test 200!"<<std::endl;
+
+  // // Return descriptor.
+  //################ BUG ISOLATION ##############################
+  Eigen::VectorXf fpfh_vec(3*nr_subdiv);
+  std::cout<<"size(fpfh_vec): "<<fpfh_vec.size()<<" size(hist_f1): "<<hist_f1.size()<<" size(hist_f2): "<<hist_f2.size()<<" size(hist_f3): "<<hist_f3.size()<<std::endl;
+  fpfh_vec = hist_tot.row(0);
+  // fpfh_vec << hist_f1.row(0), hist_f2.row(0), hist_f3.row(0);
   std::cout<<"Size feature vec: "<<fpfh_vec.size()<<std::endl;
-  // std::generate(test_fpfh.begin(), test_fpfh.end(), std::rand);
-  Feature fpfh_feature("fpfh");
-  // fpfh_feature.push_back(
-  //     FeatureValue("fpfh_x", centroid.x));
-  //   fpfh_feature.push_back(
-  //     FeatureValue("fpfh_y", centroid.y));
-  // fpfh_feature.push_back(
-  // FeatureValue("fpfh_z", centroid.z));
+  //#############################################################
 
-  for (size_t j = 0u; j < fpfh_vec.size(); ++j) {
+  Feature fpfh_feature("fpfh");
+
+  // Sandbox
+  std::vector<double> test_fpfh(33, 12); 
+  std::generate(test_fpfh.begin(), test_fpfh.end(), std::rand);
+  for (size_t j = 0u; j < test_fpfh.size(); ++j){//fpfh_vec.size(); ++j) {
+
       fpfh_feature.push_back(
-      FeatureValue("fpfh_" + std::to_string(j), fpfh_vec[j]));
+      FeatureValue("fpfh_" + std::to_string(j), test_fpfh[j])); // fpfh_vec[j]));
   }
+
+  std::cout<<"Size feature: "<<test_fpfh.size()<<" "<<fpfh_feature.size()<<std::endl;
 
   features->replaceByName(fpfh_feature);
 
   double secondsPassed =  (clock() - startTime) / CLOCKS_PER_SEC;
-  std::cout<<"It took: "<<secondsPassed<<" seconds!"<<std::endl;
 
 }
 
