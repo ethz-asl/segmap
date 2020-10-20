f331746b569ea8a9729694b2f75757b90c4b37d3
Nikhilesh Alatur <nikhilesh.alatur@ethz-asl.ch>
2020-10-20 19:57:47 +0200

    Compute distances among descriptors and save to CSV file.

--- segmatch/test/fpfh_debug.cpp
+++ segmatch/test/fpfh_debug.cpp
@@ -2,14 +2,11 @@
 // It split the cloud into segments, extracts the FPFH descriptor from each segment and saves the segment.
 
 #include <algorithm>
+#include<fstream>
 #include <iostream>
 
-#include "segmatch/descriptors/fpfh.hpp"
-#include "segmatch/parameters.hpp"
-#include <rosbag/bag.h>
-#include <rosbag/view.h>
-#include <sensor_msgs/PointCloud2.h>
-
+#include <Eigen/Core>
+#include <Eigen/Eigenvalues>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/conversions.h>
 #include <pcl/features/fpfh.h>
@@ -19,8 +16,13 @@
 #include <pcl_ros/transforms.h>
 #include <pcl/visualization/pcl_plotter.h>
 #include <pcl/visualization/pcl_visualizer.h>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+#include <sensor_msgs/PointCloud2.h>
 
+#include "segmatch/descriptors/fpfh.hpp"
 #include "segmatch/features.hpp"
+#include "segmatch/parameters.hpp"
 #include "segmatch/segmented_cloud.hpp"
 
 int main(int argc, char **argv) {
@@ -45,7 +47,7 @@ int main(int argc, char **argv) {
 	if(input==NULL)
 	{
 		std::cout<<"Something went wrong!"<<std::endl;
-		return;
+		return 0;
 	}
 	
 	// Conver to PCL standard.
@@ -58,7 +60,7 @@ int main(int argc, char **argv) {
 
 	bag.close();
 
-	pcl::io::savePCDFileASCII ("/home/nikhilesh/Documents/segments/test_pcd.pcd", *temp_cloud);
+	pcl::io::savePCDFileASCII ("/home/nikhilesh/Documents/segments/fpfh_output/full_pcd.pcd", *temp_cloud);
 
 	// Count number of segments (ID = intensity).
 	std::vector<int> segment_ids;
@@ -98,14 +100,16 @@ int main(int argc, char **argv) {
 	{
 		if(cloud_segments[i].size()>0)
 		{
-			std::cout<<"Cloud No. "<<i<<" has "<<cloud_segments[i].size()<<" points."<<std::endl;
-			pcl::io::savePCDFileASCII ("/home/nikhilesh/Documents/segments/segment" + std::to_string(i)+".pcd", cloud_segments[i]);
+			// std::cout<<"Cloud No. "<<i<<" has "<<cloud_segments[i].size()<<" points."<<std::endl;
+			pcl::io::savePCDFileASCII ("/home/nikhilesh/Documents/segments/fpfh_output/segment" + std::to_string(i)+".pcd", cloud_segments[i]);
 		}
 	}
 
 	// Extract the FPFH descriptor for each segment.
 	segmatch::DescriptorsParameters dummy_params;
 	segmatch::FpfhDescriptor fpfh_tester(dummy_params);
+	Eigen::MatrixXf fpfh_descriptors(33,cloud_segments.size());
+	fpfh_descriptors.setZero();
 
 	for(int i=0;i<cloud_segments.size();i++)
 	{
@@ -120,19 +124,19 @@ int main(int argc, char **argv) {
 		// Compute FPFH.
 		segmatch::Features fpfh_features;
 		fpfh_tester.describe(seg, &fpfh_features);
-		std::cout<<"Computed Features: "<<fpfh_features.size()<<std::endl;
+		// std::cout<<"Computed Features: "<<fpfh_features.size()<<std::endl;
 
 		// Extract descriptor.
 		std::vector<segmatch::FeatureValueType> descriptor = fpfh_features.asVectorOfValues();
-		std::cout<<"Size Descriptor: "<<descriptor.size()<<std::endl;
-
+		// std::cout<<"Size Descriptor: "<<descriptor.size()<<std::endl;
 
 		// // Visualize histogram.
   		// pcl::FPFHSignature33 fpfh_descriptor;
-		// for (int k=0; k<33; k++)
-		// {
-		//   fpfh_descriptor.histogram[k] = float(descriptor[k]);
-		// }
+		for (int k=0; k<33; k++)
+		{
+		   fpfh_descriptors(k,i) = float(descriptor[k]);
+		  // fpfh_descriptor.histogram[k] = float(descriptor[k]);
+		}
 		// pcl::PointCloud<pcl::FPFHSignature33>::Ptr descriptors(new pcl::PointCloud<pcl::FPFHSignature33>());
 		// descriptors->push_back(fpfh_descriptor);
 		// std::cout<<"Size Hist "<<descriptors->size()<<std::endl;
@@ -141,6 +145,31 @@ int main(int argc, char **argv) {
 		// plotter.plot();
 	}
 
+	// std::cout<<fpfh_descriptors<<std::endl;
+
+	// Compute cross distances.
+	Eigen::MatrixXf distance_matrix(fpfh_descriptors.cols(),fpfh_descriptors.cols());
+	for(int i=0;i<fpfh_descriptors.cols();i++)
+	{
+		for(int j=0;j<fpfh_descriptors.cols();j++)
+			{
+				// Compute norm(i,j).
+				double uu;
+				float distance = (fpfh_descriptors.col(i)-fpfh_descriptors.col(j)).norm();
+				distance_matrix(i,j) = distance;
+			}
+	}
+
+	// std::cout<<distance_matrix<<std::endl;
+
+	// Save cross-distance matrix to CSV file.
+	const static Eigen::IOFormat CSVFormat(Eigen::FullPrecision, Eigen::DontAlignCols, ", ", "\n");
+	std::ofstream file("/home/nikhilesh/Documents/segments/fpfh_output/cross_distances.csv");
+    if (file.is_open())
+    {
+        file << distance_matrix.format(CSVFormat);
+        file.close();
+    }
 
 
 	// Retrieve feature.
