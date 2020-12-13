764e1c4e9a7609c22b228fdef673e4ec32fdff8f
Andrei Cramariuc <andrei.cramariuc@yahoo.com>
2020-12-13 13:17:25 +0100

    Attempt to integrate pointnet2

--- segmappy/segmappy/core/preprocessor.py
+++ segmappy/segmappy/core/preprocessor.py
@@ -11,7 +11,6 @@ class Preprocessor(object):
         augment_remove_plane_min=0.0,
         augment_remove_plane_max=0.0,
         augment_jitter=0.0,
-        align="none",
         voxelize=True,
         scale_method="fixed",
         center_method="none",
@@ -27,24 +26,15 @@ class Preprocessor(object):
         self.augment_remove_plane_min = augment_remove_plane_min
         self.augment_remove_plane_max = augment_remove_plane_max
         self.augment_angle = augment_angle
+        self.augment_scale_low = 0.8
+        self.augment_scale_high = 1.25
+        self.augment_shift_range = 0.1
         self.augment_jitter = augment_jitter
 
-        self.align = align
-        self.voxelize = voxelize
-        self.scale_method = scale_method
-        self.center_method = center_method
-        self.scale = np.array(scale)
-        self.voxels = np.array(voxels)
-        self.remove_mean = remove_mean
-        self.remove_std = remove_std
         self.batch_size = batch_size
-        self.scaler_train_passes = scaler_train_passes
-        self.color = True
+        self.color = False
         self.n_semcls = 35
-        self._scaler_exists = False
-
-        min_voxel_side_length_m = 0.1
-        self.min_scale = self.voxels * min_voxel_side_length_m
+        self.n_points = 1024
 
         self.last_scales = []
 
@@ -56,18 +46,6 @@ class Preprocessor(object):
         self.segments_color = segments_color
         self.segments_class = segments_class
         self.classes = np.array(classes)
-
-        if self.align == "robot":
-            assert positions is not None
-            self.segments = self._align_robot(self.segments, positions)
-
-        # check if we need to train a scaler
-        if self.remove_mean or self.remove_std:
-            if scaler_path is None:
-                assert train_ids is not None
-                self._train_scaler(train_ids)
-            else:
-                self.load_scaler(scaler_path)
 
     def get_processed(self, segment_ids, train=True, normalize=True):
         batch_segments = []
@@ -84,35 +62,29 @@ class Preprocessor(object):
         return batch_segments, batch_classes
 
     def process(self, segments, segments_color, segments_class, train=True, normalize=True):
-        # augment through distorsions
-        if train and self.augment_remove_random_max > 0:
-            segments, segments_color, segments_class = self._augment_remove_random(segments, segments_color, segments_class)
-
         if train and self.augment_remove_plane_max > 0:
-            segments, segments_color, segments_class = self._augment_remove_plane(segments, segments_color, segments_class)
+            segments, segments_color, segments_class = \
+                self._augment_remove_plane(
+                segments, segments_color, segments_class)
 
-        # align after distorsions
-        if self.align == "eigen":
-            segments = self._align_eigen(segments)
+        segments, segments_color, segments_class = self._select_npoints(
+            segments, segments_color, segments_class)
 
-        # augment rotation
-        if train and self.augment_angle > 0:
-            segments = self._augment_rotation(segments)
+        segments = np.array(segments)
+        segments_color = np.array(segments_color)
+        segments_class = np.array(segments_class)
 
-        if self.voxelize:
-            # rescale coordinates and center
-            segments = self._rescale_coordinates(segments)
+        segments = self._normalize_coordinates(segments)
 
-            # randomly displace the segment
-            if train and self.augment_jitter > 0:
-                segments = self._augment_jitter(segments)
-
-            # insert into voxel grid
-            segments = self._voxelize(segments, segments_color, segments_class)
-
-            # remove mean and/or std
-            if normalize and self._scaler_exists:
-                segments = self._normalize_voxel_matrix(segments)
+        if train:
+            if self.augment_angle > 0:
+                segments = self._augment_rotation_yaw(segments)
+            # TODO(smauq): augment by adding tiny rotations in xyz
+            segments = self._augment_scale(segments)
+            segments = self._augment_shift(segments)
+            # TODO(smauq): augment by random point jitter
+            segments, segments_color, segments_class = self._shuffle_points(
+                segments, segments_color, segments_class)
 
         return segments
 
@@ -121,6 +93,36 @@ class Preprocessor(object):
             return self.n_batches_train
         else:
             return self.n_batches_test
+
+    def _select_npoints(self, segments, segments_color, segments_class):
+        for i in range(len(segments)):
+            idx = np.random.choice(segments[i].shape[0], self.n_points)
+
+            segments[i] = segments[i][idx]
+            segments_color[i] = segments_color[i][idx]
+            segments_class[i] = segments_class[i][idx]
+
+        return segments, segments_color, segments_class
+
+    def _normalize_coordinates(self, segments):
+        centroids = np.mean(segments, axis=1)
+        segments = segments[:, :] - centroids[:, None, :]
+        max_dists = np.max(np.sqrt(np.sum(segments**2, axis=2)), axis=1)
+        segments = segments / max_dists[:, None, None]
+        self.last_scales = max_dists
+
+        return segments
+
+    def _shuffle_points(self, segments, segments_color, segments_class):
+        for i in range(segments.shape[0]):
+            idx = np.arange(segments.shape[1])
+            np.random.shuffle(idx)
+
+            segments[i] = segments[i][idx]
+            segments_color[i] = segments_color[i][idx]
+            segments_class[i] = segments_class[i][idx]
+
+        return segments, segments_color, segments_class
 
     # create rotation matrix that rotates point around
     # the origin by an angle theta, expressed in radians
@@ -133,91 +135,17 @@ class Preprocessor(object):
 
         return np.array(R_z)
 
-    # align according to the robot's position
-    def _align_robot(self, segments, positions):
-        aligned_segments = []
-        for i, seg in enumerate(segments):
-            center = np.mean(seg, axis=0)
-
-            robot_pos = positions[i] - center
-            seg = seg - center
-
-            # angle between robot and x-axis
-            angle = np.arctan2(robot_pos[1], robot_pos[0])
-
-            # align the segment so the robots perspective is along the x-axis
-            inv_rotation_matrix_z = self._get_rotation_matrix_z(angle)
-            aligned_seg = np.dot(seg, inv_rotation_matrix_z)
-
-            aligned_segments.append(aligned_seg)
-
-        return aligned_segments
-
-    def _align_eigen(self, segments):
-        aligned_segments = []
-        for segment in segments:
-            # Calculate covariance
-            center = np.mean(segment, axis=0)
-
-            covariance_2d = np.cov(segment[:, :2] - center[:2], rowvar=False, bias=True)
-
-            eigenvalues, eigenvectors = np.linalg.eig(covariance_2d)
-            alignment_rad = np.arctan2(eigenvectors[1, 0], eigenvectors[0, 0])
-
-            if eigenvalues[0] < eigenvalues[1]:
-                alignment_rad = alignment_rad + np.pi / 2
-
-            inv_rotation_matrix_z = self._get_rotation_matrix_z(alignment_rad)
-            aligned_segment = np.dot(segment, inv_rotation_matrix_z)
-
-            y_center = np.mean(segment[:, 1])
-            n_below = np.sum(segment[:, 1] < y_center)
-
-            if n_below < segment.shape[0] / 2:
-                alignment_rad = alignment_rad + np.pi
-                inv_rotation_matrix_z = self._get_rotation_matrix_z(np.pi)
-                aligned_segment = np.dot(aligned_segment, inv_rotation_matrix_z)
-
-            aligned_segments.append(aligned_segment)
-
-        return aligned_segments
-
     # augment with multiple rotation of the same segment
-    def _augment_rotation(self, segments):
+    def _augment_rotation_yaw(self, segments):
         angle_rad = self.augment_angle * np.pi / 180
-
-        augmented_segments = []
-        for segment in segments:
+        for i in range(len(segments)):
             rotation = np.random.uniform(-angle_rad, angle_rad)
-            segment = np.dot(segment, self._get_rotation_matrix_z(rotation))
-            augmented_segments.append(segment)
+            segments[i] = np.matmul(
+                segments[i], self._get_rotation_matrix_z(rotation))
 
-        return augmented_segments
-
-    def _augment_remove_random(self, segments, segments_color, segments_class):
-        augmented_segments = []
-        for i, segment in enumerate(segments):
-            # percentage of points to remove
-            remove = (
-                np.random.random()
-                * (self.augment_remove_random_max - self.augment_remove_random_min)
-                + self.augment_remove_random_min
-            )
-
-            # randomly choose the points
-            idx = np.arange(segment.shape[0])
-            np.random.shuffle(idx)
-            idx = idx[int(idx.size * remove) :]
-
-            segment = segment[idx]
-            augmented_segments.append(segment)
-            segments_color[i] = segments_color[i][idx]
-            segments_class[i] = segments_class[i][idx]
-
-        return augmented_segments, segments_color, segments_class
+        return segments
 
     def _augment_remove_plane(self, segments, segments_color, segments_class):
-        augmented_segments = []
         for i, segment in enumerate(segments):
             # center segment
             center = np.mean(segment, axis=0)
@@ -235,18 +163,16 @@ class Preprocessor(object):
                 # find an offset that removes a desired amount of points
                 found = False
                 plane_offsets = np.linspace(
-                    -np.max(self.scale), np.max(self.scale), 100
+                    -np.max(segment), np.max(segment), 100
                 )
+
                 np.random.shuffle(plane_offsets)
                 for plane_offset in plane_offsets:
                     keep = sign + plane_offset > 0
                     remove_percentage = 1 - (np.sum(keep) / float(keep.size))
 
-                    if (
-                        remove_percentage > self.augment_remove_plane_min
-                        and remove_percentage < self.augment_remove_plane_max
-                    ):
-                        segment = segment[keep]
+                    if remove_percentage > self.augment_remove_plane_min and remove_percentage < self.augment_remove_plane_max:
+                        segments[i] = segment[keep] + center
                         segments_color[i] = segments_color[i][keep]
                         segments_class[i] = segments_class[i][keep]
                         found = True
@@ -255,10 +181,22 @@ class Preprocessor(object):
                 if found:
                     break
 
-            segment = segment + center
-            augmented_segments.append(segment)
+        return segments, segments_color, segments_class
 
-        return augmented_segments, segments_color, segments_class
+    def _augment_scale(self, segments):
+        scales = np.random.uniform(
+            self.augment_scale_low, self.augment_scale_high, segments.shape[0])
+        segments *= scales[:, None, None]
+
+        return segments
+
+    def _augment_shift(self, segments):
+        shifts = np.random.uniform(
+            -self.augment_shift_range, self.augment_shift_range,
+            (segments.shape[0] , segments.shape[2]))
+        segments += shifts[:, None, :]
+
+        return segments
 
     def _augment_jitter(self, segments):
         jitter_segments = []
@@ -270,102 +208,3 @@ class Preprocessor(object):
             jitter_segments.append(segment)
 
         return jitter_segments
-
-    def _rescale_coordinates(self, segments):
-        # center corner to origin
-        centered_segments = []
-        for segment in segments:
-            segment = segment - np.min(segment, axis=0)
-            centered_segments.append(segment)
-        segments = centered_segments
-
-        # store the last scaling factors that were used
-        self.last_scales = []
-
-        # rescale coordinates to fit inside voxel matrix
-        rescaled_segments = []
-        for segment in segments:
-            # choose scale
-            if self.scale_method == "fixed":
-                scale = self.scale
-                segment = segment / scale * (self.voxels - 1)
-            elif self.scale_method == "aspect":
-                scale = np.tile(np.max(segment), 3)
-                segment = segment / scale * (self.voxels - 1)
-            elif self.scale_method == "fit":
-                scale = np.max(segment, axis=0)
-                thresholded_scale = np.maximum(scale, self.min_scale)
-                segment = segment / thresholded_scale * (self.voxels - 1)
-
-            # recenter segment
-            if self.center_method != "none":
-                if self.center_method == "mean":
-                    center = np.mean(segment, axis=0)
-                elif self.center_method == "min_max":
-                    center = np.max(segment, axis=0) / 2.0
-
-                segment = segment + (self.voxels - 1) / 2.0 - center
-
-            self.last_scales.append(scale)
-            rescaled_segments.append(segment)
-
-        return rescaled_segments
-
-    def _voxelize(self, segments, segments_color, segments_class):
-        if self.color:
-            voxelized_segments = np.zeros((len(segments),) + tuple(self.voxels) + (3 + self.n_semcls,))
-        else:
-            voxelized_segments = np.zeros((len(segments),) + tuple(self.voxels) + (1,))
-
-        for i, segment in enumerate(segments):
-            # remove out of bounds points
-            keep = np.logical_and(np.all(segment < self.voxels, axis=1), np.all(segment >= 0, axis=1))
-            segment = segment[keep]
-
-            # round coordinates
-            segment = segment.astype(np.int)
-
-            # fill voxel grid
-            if self.color:
-                voxelized_segments[i, segment[:, 0], segment[:, 1], segment[:, 2], :3] = segments_color[i][keep]
-                voxelized_segments[i, segment[:, 0], segment[:, 1], segment[:, 2], segments_class[i][keep] + 3] = 1
-            else:
-                voxelized_segments[i, segment[:, 0], segment[:, 1], segment[:, 2]] = 1
-
-        return voxelized_segments
-
-    def _train_scaler(self, train_ids):
-        from sklearn.preprocessing import StandardScaler
-
-        scaler = StandardScaler(with_mean=self.remove_mean, with_std=self.remove_std)
-
-        for p in range(self.scaler_train_passes):
-            for i in np.arange(0, len(train_ids), self.batch_size):
-                segment_ids = train_ids[i : i + self.batch_size]
-                segments, _ = self.get_processed(segment_ids)
-                segments = np.reshape(segments, (segments.shape[0], -1))
-                scaler.partial_fit(segments)
-
-        self._scaler = scaler
-        self._scaler_exists = True
-
-    # remove mean and std
-    def _normalize_voxel_matrix(self, segments):
-        segments = np.reshape(segments, (segments.shape[0], -1))
-        segments = self._scaler.transform(segments)
-        segments = np.reshape(segments, (segments.shape[0],) + tuple(self.voxels))
-
-        return segments
-
-    def save_scaler(self, path):
-        import pickle
-
-        with open(path, "w") as fp:
-            pickle.dump(self._scaler, fp)
-
-    def load_scaler(self, path):
-        import pickle
-
-        with open(path, "r") as fp:
-            self._scaler = pickle.load(fp)
-            self._scaler_exists = True
---
+++ segmappy/segmappy/tf_ops/grouping/tf_grouping_compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+/usr/local/cuda/bin/nvcc tf_grouping_g.cu -o tf_grouping_g.cu.o \
+    -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+
+TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
+
+g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so \
+    -shared -fPIC -I$TF_INC -I$TF_INC/external/nsync/public \
+    -I/usr/local/cuda-10.0/include \
+    -lcudart -L /usr/local/cuda-10.0/lib64/ \
+    -L$TF_LIB -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
